fe2fce7ee64070f46ce3323caf8aeb0e1a44ef21
Marcello Nicollette <marcello@wabawaba.com.br>
2025-05-14 11:15:41 -0300

    ajsute vite.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['../resources/css/app.css', '../resources/js/app.js'],
+            input: ['/public_html/resources/css/app.css', '/public_html/resources/js/app.js'],
             refresh: true,
         }),
     ],
