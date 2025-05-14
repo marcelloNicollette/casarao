5b030d8ab653fe9da24dba0256a5c39184df8c78
Marcello Nicollette <marcello@wabawaba.com.br>
2025-05-14 11:20:00 -0300

    ajuste vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['/public_html/resources/css/app.css', '/public_html/resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
-    resolve: {
-        alias: {
-            crypto: 'crypto-browserify'
-        }
-    }
+    ]
 });
